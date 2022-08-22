3fe86ad3536398dfcb4a6338274f431b3e91e9c7
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-22 14:19:54 -0300

    first commit

---
+++ src/main/java/br/com/luan/registros/controller/HelloController.java
@@ -0,0 +1,32 @@
+package br.com.luan.registros.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello");  // nome do arquivo html a ser renderizado/exibido
+        mv.addObject("nome", "Maria!");
+        return mv;  // o Spring vai renderizar o arquivo templates/hello.html
+    }
+
+
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Zezinho!");
+        return "hello";  // o Spring vai renderizar o arquivo templates/hello.html
+    }
+
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Luan");
+        return "hello";  // o Spring vai renderizar o arquivo templates/hello.html
+    }
+}
---
+++ src/test/java/br/com/luan/registros/RegistrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luan.registros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
