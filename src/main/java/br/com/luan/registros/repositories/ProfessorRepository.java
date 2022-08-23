59c2278087c7e1d03d20c86bd5cfc950e51a5554
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-23 10:42:00 -0300

    add form new professor

--- src/main/java/br/com/luan/registros/repositories/ProfessorRepository.java
+++ src/main/java/br/com/luan/registros/repositories/ProfessorRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProfessorRepository extends JpaRepository<Professor, Long> {
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
 }
